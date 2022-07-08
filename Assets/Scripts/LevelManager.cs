05d308d72f506c5d5bf48ec6731b8ae49f15a7d7
Priyanka Wakchaure <priyankawakchaure89@gmail.com>
2022-07-08 08:18:04 +0530

    Added basic UI

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
